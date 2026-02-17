00dd21c723f0613fcd8ac82ee4593e93aa9abce3
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-17 11:59:28 -0300

    Adds math endpoint for sum method

---
+++ RestASPNet/RestASPNet/RestASPNet/Controllers/MathController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestASPNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+            var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(sum);
+
+            } else
+            {
+                return BadRequest("Invalid Input");
+            }
+        }
+
+        private decimal ConvertToDecimal(string numberString)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(
+                numberString,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue))
+            {
+                return decimalValue;
+            }
+            else 
+            {
+                return 0; 
+            }
+        }
+
+        private bool IsNumeric(string stringNumber)
+        {
+            decimal decimalNumber = 0;
+            bool isNumber = decimal.TryParse(
+                stringNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalNumber);
+            return isNumber;
+        }
+    }
+}
