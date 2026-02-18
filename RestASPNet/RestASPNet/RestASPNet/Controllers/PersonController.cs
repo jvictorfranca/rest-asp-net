da008afc5d723e5db4c4a38e6ccb2e6f1c57977c
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-18 16:02:59 -0300

    Adds routes for other https verbs

--- RestASPNet/RestASPNet/RestASPNet/Controllers/PersonController.cs
+++ RestASPNet/RestASPNet/RestASPNet/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestASPNet.Controllers.Model;
+using RestASPNet.Services;
 
 namespace RestASPNet.Controllers
 {
@@ -7,5 +9,59 @@ namespace RestASPNet.Controllers
     [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonServices _personServices;
+
+        public PersonController(IPersonServices personServices)
+        {
+            _personServices = personServices;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personServices.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personServices.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+         {
+            var createdPerson = _personServices.Create(person);
+            if (createdPerson == null)
+            {
+                return NotFound();
+            }
+            return Ok(createdPerson);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            var createdPerson = _personServices.Update(person);
+            if (createdPerson == null)
+            {
+                return NotFound();
+            }
+            return Ok(createdPerson);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personServices.Delete(id);
+            return NoContent();
+        }
+        
     }
 }
---
+++ RestASPNet/RestASPNet/RestASPNet/Services/Impl/PersonServicesImpl.cs
@@ -0,0 +1,52 @@
+﻿using RestASPNet.Controllers.Model;
+
+namespace RestASPNet.Services.Impl
+{
+    public class PersonServicesImpl : IPersonServices
+    {
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i<8; i++)
+            {
+                persons.Add(MockPerson(i));
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            var person = MockPerson((int) id);
+
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = new Random().Next(1, 1000),
+                FirstName = "Leandro " + i,
+                LastName = "Costa " + i,
+                Adress = "Rua dos Bobos, nº " + i,
+                Gender = i % 2 == 0 ? "Male" : "Female"
+            };
+        }
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            // No content
+        }
+
+
+    }
+}
