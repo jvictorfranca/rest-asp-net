0e1acc842c75119a7c17919c9c8adc2f24c9e2af
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-18 14:37:33 -0300

    Create class and interface for person

---
+++ RestASPNet/RestASPNet/RestASPNet/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestASPNet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+    }
+}
---
+++ RestASPNet/RestASPNet/RestASPNet/Services/IPersonServices.cs
@@ -0,0 +1,13 @@
+﻿using RestASPNet.Controllers.Model;
+
+namespace RestASPNet.Services
+{
+    public interface IPersonServices
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
