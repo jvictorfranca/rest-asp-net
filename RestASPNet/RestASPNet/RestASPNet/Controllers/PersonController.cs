6ebbd3aafe214fceb18f6e92c0b27125fc90044d
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-19 19:23:41 -0300

    Adds logging with Serilog extension

--- RestASPNet/RestASPNet/RestASPNet/Controllers/PersonController.cs
+++ RestASPNet/RestASPNet/RestASPNet/Controllers/PersonController.cs
@@ -10,24 +10,29 @@ namespace RestASPNet.Controllers
     public class PersonController : ControllerBase
     {
         private readonly IPersonServices _personServices;
+        private readonly ILogger<PersonController> _logger;
 
-        public PersonController(IPersonServices personServices)
+        public PersonController(IPersonServices personServices, ILogger<PersonController> logger)
         {
             _personServices = personServices;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation("Fetching all people");
             return Ok(_personServices.FindAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(long id)
         {
+            _logger.LogInformation("Fetching person with ID {id}", id);
             var person = _personServices.FindById(id);
             if (person == null)
             {
+                _logger.LogWarning("Person with ID {id} not found", id);
                 return NotFound();
             }
             return Ok(person);
@@ -37,9 +42,11 @@ namespace RestASPNet.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Person person)
          {
+            _logger.LogInformation("Creating new person {firstName}", person.FirstName);
             var createdPerson = _personServices.Create(person);
             if (createdPerson == null)
             {
+                _logger.LogError("Failed to create person {firstName}", person.FirstName);
                 return NotFound();
             }
             return Ok(createdPerson);
@@ -48,17 +55,21 @@ namespace RestASPNet.Controllers
         [HttpPut]
         public IActionResult Put([FromBody] Person person)
         {
+            _logger.LogInformation("Updating person with ID {id}", person.Id);
             var createdPerson = _personServices.Update(person);
             if (createdPerson == null)
             {
+                _logger.LogError("Person with ID {id} not found for update", person.Id);
                 return NotFound();
             }
+            _logger.LogDebug("Person with ID {id} updated successfully", person.Id);
             return Ok(createdPerson);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            _logger.LogInformation("Fetching person with ID {id}", id);
             _personServices.Delete(id);
             return NoContent();
         }
---
+++ RestASPNet/RestASPNet/RestASPNet/Controllers/TestLogsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestASPNet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestLogsController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TestLogsController(ILogger<TestLogsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult LogTest()
+        {
+            _logger.LogDebug("this is a debug log");
+            _logger.LogInformation("This is an information log.");
+            _logger.LogWarning("This is a warning log.");
+            _logger.LogError("This is an error log.");
+            _logger.LogCritical("This is a critical log");
+            return Ok("Logs have been generated. Check the console output.");
+        }
+    }
+}
