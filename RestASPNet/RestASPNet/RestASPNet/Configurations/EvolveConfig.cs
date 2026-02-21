4352a85c7abe8c9970a41b622d38845af3a6e602
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-21 12:09:24 -0300

    Add Evolve migrations

---
+++ RestASPNet/RestASPNet/RestASPNet/Configurations/EvolveConfig.cs
@@ -0,0 +1,36 @@
+﻿using EvolveDb;
+using Microsoft.Data.SqlClient;
+using Serilog;
+
+namespace RestASPNet.Configurations
+{
+    public static class EvolveConfig
+    {
+        public static IServiceCollection AddEvolveConfiguration(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                var connectionString = configuration["MSSQLServerSqlConnection:ConnectionString"];
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    throw new ArgumentException("Connection string not configured");
+                }
+
+                try
+                {
+                    using var evolveConnection = new SqlConnection(connectionString);
+                    var evolve = new Evolve(evolveConnection, msg => Log.Information(msg))
+                    {
+                        Locations = new List<string> { "db/migrations", "db/dataset" },
+                        IsEraseDisabled = true,
+                    };
+                    evolve.Migrate();
+                }
+                catch (Exception ex) {
+                    Log.Error("Database migration failed: {ErrorMessage}", ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
