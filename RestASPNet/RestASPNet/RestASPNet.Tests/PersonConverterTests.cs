98818cc3c1dad509503b74117c7a3dd366dc1d7e
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-25 18:30:33 -0300

    Add unit tests from PersonDTO to Person

--- RestASPNet/RestASPNet/RestASPNet.Tests/PersonConverterTests.cs
+++ RestASPNet/RestASPNet/RestASPNet.Tests/PersonConverterTests.cs
@@ -59,7 +59,7 @@ namespace RestASPNet.Tests
         }
 
         [Fact]
-        public void Parse_SNullPersonDTOShouldReturnNull()
+        public void Parse_NullPersonDTOShouldReturnNull()
         {
             PersonDTO personDTO = null;
 
@@ -70,6 +70,177 @@ namespace RestASPNet.Tests
             // Assert
 
             resultPerson.Should().BeNull();
+        }
+
+        // Person Should parse
+
+        [Fact]
+        public void Parse_ShouldConvertPersonToPersonDTO()
+        {
+            // Arrange
+            var person = new Person
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Gender = "Male",
+                Adress = "123 Main"
+            };
+
+            var expectedPersonDTO = new PersonDTO
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Gender = "Male",
+                Adress = "123 Main"
+            };
+
+            // Act
+
+            var resultPerson = _converter.Parse(person);
+
+            // Assert
+
+            resultPerson.Should().NotBeNull();
+            resultPerson.Id.Should().Be(expectedPersonDTO.Id);
+            resultPerson.FirstName.Should().Be(expectedPersonDTO.FirstName);
+            resultPerson.LastName.Should().Be(expectedPersonDTO.LastName);
+            resultPerson.Gender.Should().Be(expectedPersonDTO.Gender);
+            resultPerson.Adress.Should().Be(expectedPersonDTO.Adress);
+
+            resultPerson.Should().BeEquivalentTo(expectedPersonDTO, options => options.Excluding(person => person.Gender));
+
+        }
+
+        [Fact]
+        public void Parse_NullPersonShouldReturnNull()
+        {
+            // Arrange
+            Person person = null;
+
+            // Act
+
+            var resultPerson = _converter.Parse(person);
+
+            // Assert
+
+            resultPerson.Should().BeNull();
+        }
+
+        [Fact]
+        public void ParseList_ShouldConvertPersonDTOListToPersonList()
+        {
+            // Arrange
+
+            var dtoList = new List<PersonDTO>
+            {
+                new PersonDTO
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Gender = "Male",
+                    Adress = "123 Main"
+                },
+                new PersonDTO
+                {
+                    Id = 2,
+                    FirstName = "John2",
+                    LastName = "Doe2",
+                    Gender = "Male2",
+                    Adress = "123 Main2"
+                }
+            };
+
+            var expectedPersonList = new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Gender = "Male",
+                    Adress = "123 Main"
+                },
+                new Person
+                {
+                    Id = 2,
+                    FirstName = "John2",
+                    LastName = "Doe2",
+                    Gender = "Male2",
+                    Adress = "123 Main2"
+                }
+            };
+
+            // Act
+
+            var personList = _converter.ParseList(dtoList);
+
+            // Assert
+
+            personList.Should().NotBeNull();
+            personList.Should().HaveCount(2);
+            personList.Should().BeEquivalentTo(expectedPersonList);
+            personList[0].LastName.Should().Be("Doe");
+
+        }
+
+        [Fact]
+        public void ParseList_ShouldConvertPersonListToPersonDTOList()
+        {
+            // Arrange
+
+            var dtoList = new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Gender = "Male",
+                    Adress = "123 Main"
+                },
+                new Person
+                {
+                    Id = 2,
+                    FirstName = "John2",
+                    LastName = "Doe2",
+                    Gender = "Male2",
+                    Adress = "123 Main2"
+                }
+            };
+
+            var expectedPersonList = new List<PersonDTO>
+            {
+                new PersonDTO
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Gender = "Male",
+                    Adress = "123 Main"
+                },
+                new PersonDTO
+                {
+                    Id = 2,
+                    FirstName = "John2",
+                    LastName = "Doe2",
+                    Gender = "Male2",
+                    Adress = "123 Main2"
+                }
+            };
+
+            // Act
+
+            var personList = _converter.ParseList(dtoList);
+
+            // Assert
+
+            personList.Should().NotBeNull();
+            personList.Should().HaveCount(2);
+            personList.Should().BeEquivalentTo(expectedPersonList);
+
         }
     }
 }
