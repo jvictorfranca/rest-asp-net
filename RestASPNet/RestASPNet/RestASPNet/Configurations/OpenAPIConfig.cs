a77350b5722826c37a7c5fda8c0265dd2454fb0b
Joao Franca <jvictorfranca@yahoo.com.br>
2026-02-28 13:01:39 -0300

    Adds Configuration for Scallar and Swagger OpenAPI config

---
+++ RestASPNet/RestASPNet/RestASPNet/Configurations/OpenAPIConfig.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OpenApi;
+
+namespace RestASPNet.Configurations
+{
+    public static class OpenAPIConfig
+    {
+        private static readonly string App_Name = "Asp net 2026 with Docker and Kubernetes";
+        private static readonly string App_Description = "API RESTFUL developed on Udemy course";
+
+        public static IServiceCollection AddOpenAPIConfig(this IServiceCollection services)
+        {
+
+            services.AddSingleton(new OpenApiInfo
+            {
+                Title = App_Name,
+                Version = "v1",
+                Description = App_Description,
+                Contact = new OpenApiContact
+                {
+                    Name = "Joao Franca",
+                    Email = "jvictorfranca@yahoo.com.br"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT",
+
+                }
+            });
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+            //    {
+            //        Title = App_Name,
+            //        Version = "v1",
+            //        Description = App_Description
+            //    });
+            //});
+            return services;
+        }
+    }
+}
