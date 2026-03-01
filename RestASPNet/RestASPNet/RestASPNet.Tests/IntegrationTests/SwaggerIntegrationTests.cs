144e1e372e93abedee36ea9b9cadf3bb671652d9
Joao Franca <jvictorfranca@yahoo.com.br>
2026-03-01 17:19:48 -0300

    Adds first integration test

---
+++ RestASPNet/RestASPNet/RestASPNet.Tests/IntegrationTests/SwaggerIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using RestASPNet.Tests.IntegrationTests.Tools;
+
+namespace RestASPNet.Tests.IntegrationTests
+{
+    public class SwaggerIntegrationTests : IClassFixture<SQLServerFixture>
+    {
+        private readonly HttpClient _httpClient;
+
+        public SwaggerIntegrationTests(SQLServerFixture sqlServerFixture)
+        {
+            var factory = new CustomWeApplicationFactory<Program>(sqlServerFixture.ConnectionString);
+            _httpClient = factory.CreateClient
+                (
+                new Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryClientOptions
+                {
+                    BaseAddress = new Uri("http://localhost")
+                }
+                );
+        }
+
+        [Fact]
+        public async Task SwaggerUI_ShouldReturnSwaggerJson()
+        {
+            // Act
+            var response = await _httpClient.GetAsync("/swagger/v1/swagger.json");
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().NotBeNullOrEmpty();
+            content.Should().Contain("/api/person/v1");
+        }
+    }
+}
