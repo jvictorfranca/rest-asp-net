619773b5a41fdd0dbb20e07dfdbb8491f1799a6e
Joao Franca <jvictorfranca@yahoo.com.br>
2026-03-01 17:07:19 -0300

    Add web application factory

---
+++ RestASPNet/RestASPNet/RestASPNet.Tests/IntegrationTests/Tools/CustomWeApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace RestASPNet.Tests.IntegrationTests.Tools
+{
+    public class CustomWeApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        private readonly string _connectionString;
+        public CustomWeApplicationFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+            {
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    var inMemorySettings = new Dictionary<string, string>
+                    {
+                        ["MSSQLServerSqlConnection:ConnectionString"] = _connectionString
+                    };
+                    config.AddInMemoryCollection(inMemorySettings!);
+                });
+        }
+    }
+}
