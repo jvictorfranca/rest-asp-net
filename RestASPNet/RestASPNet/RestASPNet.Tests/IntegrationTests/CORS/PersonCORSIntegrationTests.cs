e6c94a9fbecb6e9b3db83b9c89497e6ba377c0e1
Joao Franca <jvictorfranca@yahoo.com.br>
2026-03-02 22:39:21 -0300

    Create CORS integration tests for PersonDTO

--- RestASPNet/RestASPNet/RestASPNet.Tests/IntegrationTests/CORS/PersonCORSIntegrationTests.cs
+++ RestASPNet/RestASPNet/RestASPNet.Tests/IntegrationTests/CORS/PersonCORSIntegrationTests.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text;
 
+// [assembly: CollectionBehavior(DisableTestParallelization = true)]
 namespace RestASPNet.Tests.IntegrationTests.CORS
 {
     [TestCaseOrderer("RestASPNet.Tests.IntegrationTests.Tools.PriorityOrder", "RestASPNet.Tests")]
@@ -15,6 +16,7 @@ namespace RestASPNet.Tests.IntegrationTests.CORS
     {
 
         private readonly HttpClient _httpClient;
+        private static PersonDTO? _person;
 
         public PersonCORSIntegrationTests(SQLServerFixture sqlServerFixture)
         {
@@ -57,12 +59,14 @@ namespace RestASPNet.Tests.IntegrationTests.CORS
             var createdPerson = await response.Content.ReadFromJsonAsync<PersonDTO>();
             createdPerson.Should().NotBeNull();
             createdPerson.Id.Should().BeGreaterThan(0);
+
+            _person = createdPerson;
         }
 
         [Fact(DisplayName = "CORS - Create person with not allowed Origin")]
         [TestPriority(2)]
 
-        public async Task CreatePerson_WithDisallowedOrigin_ShouldSucceed()
+        public async Task CreatePerson_WithDisallowedOrigin_ShouldNotSucceed()
         {
             // Arrange
             AddOriginHeader("http://not-localhost:3000");
@@ -81,6 +85,44 @@ namespace RestASPNet.Tests.IntegrationTests.CORS
 
             var content = await response.Content.ReadAsStringAsync();
             content.Should().Be("CORS policy does not allow this origin.");
+        }
+
+        [Fact(DisplayName = "CORS - Get person with not allowed Origin")]
+        [TestPriority(3)]
+
+        public async Task GetPerson_WithDisallowedOrigin_ShouldNotSucceed()
+        {
+            // Arrange
+            AddOriginHeader("http://not-localhost:3000");
+
+            // Act
+            var response = await _httpClient.GetAsync("/api/person/v1/1");
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Be("CORS policy does not allow this origin.");
+        }
+
+        [Fact(DisplayName = "CORS - Get person with allowed Origin")]
+        [TestPriority(4)]
+
+        public async Task GetPerson_WithAllowedOrigin_ShouldSucceed()
+        {
+            // Arrange
+            AddOriginHeader("http://localhost:3000");
+
+            // Act
+            var response = await _httpClient.GetAsync($"/api/person/v1/{_person.Id}");
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var found = await response.Content.ReadFromJsonAsync<PersonDTO>();
+            found.Should().NotBeNull();
+            found.Id.Should().Be(_person.Id);
+            found.FirstName.Should().Be(_person.FirstName);
+            found.LastName.Should().Be(_person.LastName);
+            found.Adress.Should().Be(_person.Adress);
         }
     }
 }
