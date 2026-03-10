34d2b52725dc1d32b7039de5389baa8224c93898
Joao Franca <jvictorfranca@yahoo.com.br>
2026-03-10 18:23:07 -0300

    Adds HypermediaFilter for TryEnrich, and fixes types for IresponseEnricher

---
+++ RestASPNet/RestASPNet/RestASPNet/Hypermedia/Filters/HypermediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using RestASPNet.Hypermedia.Abstract;
+
+namespace RestASPNet.Hypermedia.Filters
+{
+    public class HypermediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = [];
+    }
+}
