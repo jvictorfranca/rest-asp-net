6632c6cae55c07661a5a4a1e7a0f10791cb8ea79
Joao Franca <jvictorfranca@yahoo.com.br>
2026-04-11 10:27:06 -0300

    Implements file upload

--- RestASPNet/RestASPNet/RestASPNet/Services/Impl/FileServicesImpl.cs
+++ RestASPNet/RestASPNet/RestASPNet/Services/Impl/FileServicesImpl.cs
@@ -18,6 +18,36 @@ namespace RestASPNet.Services.Impl
                 Directory.CreateDirectory(_basePath);
             }
         }
+        public async Task<FileDetailDTO> SaveToDisk(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                throw new Exception("File is empty");
+
+            var fileExtension = Path.GetExtension(file.FileName).ToLower();
+            if(!_allowedExtensions.Contains(fileExtension))
+                throw new Exception("File type not allowed");
+
+            var documentName = Path.GetFileName(file.FileName);
+
+            var destination = Path.Combine(_basePath, documentName);
+
+            var baseUrl = $"{_context.HttpContext.Request.Scheme}://{_context.HttpContext.Request.Host}";
+
+            var fileDetail = new FileDetailDTO
+            {
+                DocumentName = documentName,
+                DocType = fileExtension,
+                DocUrl = $"{baseUrl}/api/file/v1/downloadFile/{documentName}"
+            };
+
+            using var stream = new FileStream(destination, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+            return fileDetail;
+
+
+        }
 
         public byte[] GetFile(string fileName)
         {
@@ -25,10 +55,6 @@ namespace RestASPNet.Services.Impl
         }
 
 
-        public Task<FileDetailDTO> SaveToDisk(IFormFile file)
-        {
-            throw new NotImplementedException();
-        }
         public Task<List<FileDetailDTO>> SaveFilesToDisk(List<IFormFile> files)
         {
             throw new NotImplementedException();
